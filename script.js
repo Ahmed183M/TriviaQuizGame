54b713344aafa93df406a3dbfb8b38aa720a598e
Ahmed183 <ahmed1831990@gmail.com>
2021-05-03 01:15:43 +0300

    Finished

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const nextQuestion = document.getElementById("nextQuestion");
 // Final Page
 const finalPage = document.getElementById("finalPage");
 
-const playAgainText = document.getElementById("playAgainText");
+const scoreDisplay = document.getElementById("scoreDisplay");
 const playAgain = document.getElementById("playAgain");
 
 // API
@@ -88,18 +88,175 @@ const startQuiz = () => {
         });
 
         let counter = 0;
+        let score = 0;
+        let selectedAnswer = 0;
         const questionPreviewer = () => {
-            QNumber.innerHTML = "Question " + counter + 1 + " out of 10.";
-            question.innerHTML = listOfQuestions.allQuestions()[counter].questionBody;
-            counter++;
+            answer1.style.backgroundColor = "white";
+            answer1.style.color = "black";
+            answer2.style.backgroundColor = "white";
+            answer2.style.color = "black";
+            answer3.style.backgroundColor = "white";
+            answer3.style.color = "black";
+            answer4.style.backgroundColor = "white";
+            answer4.style.color = "black";
+
             if (counter == 9) {
                 nextQuestion.innerHTML = "Go to Results!"
             }
             if (counter == 10) {
+                let emoji;
+                if (score < 3) emoji = "😢";
+                else if (score >= 3 && score < 5) emoji = "🙁";
+                else if (score >= 5 && score < 10) emoji = "🙂";
+                else if (score == 10) emoji = "😎";
 
-                
+                scoreDisplay.innerHTML = "Your score was: " + score + " " + emoji;
+
                 questionPage.style.display = "none";
                 finalPage.style.display = "block";
+            } else {
+                clicked = false;
+
+                QNumber.innerHTML = "Question " + (counter + 1) + " out of 10.";
+                question.innerHTML = listOfQuestions.allQuestions()[counter].questionBody;
+
+                console.log(listOfAnswers.allAnswers());
+                let shuffledAnswers = [];
+                shuffledAnswers[0] = listOfAnswers.allAnswers()[counter].correctAnswer;
+                shuffledAnswers[1] = listOfAnswers.allAnswers()[counter].falseAnswer1;
+                shuffledAnswers[2] = listOfAnswers.allAnswers()[counter].falseAnswer2;
+                shuffledAnswers[3] = listOfAnswers.allAnswers()[counter].falseAnswer3;
+                console.log(shuffledAnswers);
+                shuffledAnswers = shuffle(shuffledAnswers);
+                console.log(shuffledAnswers);
+                answer1.innerHTML = shuffledAnswers[0];
+                answer2.innerHTML = shuffledAnswers[1];
+                answer3.innerHTML = shuffledAnswers[2];
+                answer4.innerHTML = shuffledAnswers[3];
+
+                if (selectedAnswer == 0 && counter > 0) {
+                    alert("Please Choose an Answer First!\nAnswers have been shuffled!")
+                } else {
+                    selectedAnswer = 0;
+                    const setAnswer1 = () => {
+                        if (clicked == false) {
+                            clicked = true;
+                            selectedAnswer = answer1.innerHTML;
+                            if (selectedAnswer != listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer1.style.backgroundColor = "red";
+                                answer1.style.color = "white";
+                            } else if (selectedAnswer == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer1.style.backgroundColor = "green";
+                                answer1.style.color = "white";
+                                score++;
+                            }
+
+                            console.log(selectedAnswer);
+                            console.log(listOfAnswers.allAnswers()[counter].correctAnswer);
+                            if (answer2.innerHTML == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer2.style.backgroundColor = "green";
+                                answer2.style.color = "white";
+                            } else if (answer3.innerHTML == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer3.style.backgroundColor = "green";
+                                answer3.style.color = "white";
+                            } else if (answer4.innerHTML == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer4.style.backgroundColor = "green";
+                                answer4.style.color = "white";
+                            }
+                            counter++;
+                        }
+                    }
+                    const setAnswer2 = () => {
+                        if (clicked == false) {
+                            clicked = true;
+                            selectedAnswer = answer2.innerHTML;
+                            if (selectedAnswer != listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer2.style.backgroundColor = "red";
+                                answer2.style.color = "white";
+                            } else if (selectedAnswer == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer2.style.backgroundColor = "green";
+                                answer2.style.color = "white";
+                                score++;
+                            }
+
+                            console.log(selectedAnswer);
+                            console.log(listOfAnswers.allAnswers()[counter].correctAnswer);
+                            if (answer1.innerHTML == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer1.style.backgroundColor = "green";
+                                answer1.style.color = "white";
+                            } else if (answer3.innerHTML == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer3.style.backgroundColor = "green";
+                                answer3.style.color = "white";
+                            } else if (answer4.innerHTML == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer4.style.backgroundColor = "green";
+                                answer4.style.color = "white";
+                            }
+                            counter++;
+                        }
+                    }
+                    const setAnswer3 = () => {
+                        if (clicked == false) {
+                            clicked = true;
+                            selectedAnswer = answer3.innerHTML;
+                            if (selectedAnswer != listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer3.style.backgroundColor = "red";
+                                answer3.style.color = "white";
+                            } else if (selectedAnswer == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer3.style.backgroundColor = "green";
+                                answer3.style.color = "white";
+                                score++;
+                            }
+
+                            console.log(selectedAnswer);
+                            console.log(listOfAnswers.allAnswers()[counter].correctAnswer);
+                            if (answer2.innerHTML == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer2.style.backgroundColor = "green";
+                                answer2.style.color = "white";
+                            } else if (answer1.innerHTML == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer1.style.backgroundColor = "green";
+                                answer1.style.color = "white";
+                            } else if (answer4.innerHTML == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer4.style.backgroundColor = "green";
+                                answer4.style.color = "white";
+                            }
+                            counter++;
+                        }
+                    }
+                    const setAnswer4 = () => {
+                        if (clicked == false) {
+                            clicked = true;
+                            selectedAnswer = answer4.innerHTML;
+                            if (selectedAnswer != listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer4.style.backgroundColor = "red";
+                                answer4.style.color = "white";
+                            } else if (selectedAnswer == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer4.style.backgroundColor = "green";
+                                answer4.style.color = "white";
+                                score++;
+                            }
+
+                            console.log(selectedAnswer);
+                            console.log(listOfAnswers.allAnswers()[counter].correctAnswer);
+                            if (answer2.innerHTML == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer2.style.backgroundColor = "green";
+                                answer2.style.color = "white";
+                            } else if (answer3.innerHTML == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer3.style.backgroundColor = "green";
+                                answer3.style.color = "white";
+                            } else if (answer1.innerHTML == listOfAnswers.allAnswers()[counter].correctAnswer) {
+                                answer1.style.backgroundColor = "green";
+                                answer1.style.color = "white";
+                            }
+                            counter++;
+                        }
+                    }
+                    answer1.addEventListener("click", setAnswer1);
+                    answer2.addEventListener("click", setAnswer2);
+                    answer3.addEventListener("click", setAnswer3);
+                    answer4.addEventListener("click", setAnswer4);
+
+
+                }
             }
         }
         questionPreviewer();
@@ -111,6 +268,11 @@ const startQuiz = () => {
 }
 quizStart.addEventListener("click", startQuiz);
 
+
+const reload = () => {
+    location.reload();
+}
+playAgain.addEventListener("click", reload);
 class Question {
     constructor(questionBody) {
         this.questionBody = questionBody;
